47da4f69766ccff7c39691b891c2faa1a7f77fa1
Zadiel Salas <sadielsalas35@gmail.com>
2024-11-26 15:10:09 -0400

    cambio2

--- src/components/ui/Btn1.jsx
+++ src/components/ui/Btn1.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Btn1 = (props) => {
 	return (
-		<a href={props.red} className={`inline-block ${props.color} text-sm font-medium py-3 px-6 rounded transition`}>
+		<Links to={props.red} className={`inline-block ${props.color} text-sm font-medium py-3 px-6 rounded transition`}>
 			{props.title}
-		</a>
+		</Links>
 	);
 };
 
